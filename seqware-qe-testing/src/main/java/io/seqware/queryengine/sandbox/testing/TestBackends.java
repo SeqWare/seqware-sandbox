01900e59aa4fcefc389bab01ef7c086607cc9ce9
bcso <bcso@uwaterloo.ca>
2014-01-22 16:52:43 -0500

    refactored

--- seqware-qe-testing/src/main/java/io/seqware/queryengine/sandbox/testing/TestBackends.java
+++ seqware-qe-testing/src/main/java/io/seqware/queryengine/sandbox/testing/TestBackends.java
@@ -56,11 +56,10 @@ public class TestBackends implements BackendTestInterface
         String line = new String();
         String temp = new String();
         
-        //Store this into dummy backend, a global var : Global.HBaseBackend
+        //Store the pathfield into dummy backend, a global var : Global.HBaseBackend
         testb.loadFeatureSet("/Users/bso/gene5.vcf");        
 
-
-        //Dummy JSONQuery input
+        //Convert Dummy JSONQuery input text file to single string
 		try { 
 			in = new BufferedReader(new FileReader("/Users/bso/queryJSON.txt"));
 	        while ((line = in.readLine()) != null){
@@ -72,6 +71,7 @@ public class TestBackends implements BackendTestInterface
 			e.printStackTrace();
 		}
 		
+		//Obtain matched features
 		try {
 			returned = testb.getFeatures(temp);
 		} catch (JSONException e) {
@@ -96,9 +96,11 @@ public class TestBackends implements BackendTestInterface
 			String KEY = "gene";
 			String VALUE = filePath;
 			Global.HBaseStorage.put(KEY, VALUE);
+			//State of SUCCESS
 			state.setState(0);
 			return state;
 		} else {
+			//State of NOT_SUPPORTED
 			state.setState(3);
 			return state;
 		}
@@ -113,82 +115,107 @@ public class TestBackends implements BackendTestInterface
 	public ReturnValue getFeatures(String queryJSON) throws JSONException, IOException {
 		//Read the input JSON file to seperate ArrayLists for parsing
 		ReturnValue finished = new ReturnValue();
-		
 		JSONObject jsonObOuter = new JSONObject(queryJSON);
 		JSONArray regionArray;
 		Iterator<String> OutterKeys = jsonObOuter.keys();
 		
+		//Points to input VCF file to process
+		//Points to output filepath
+		File sortedVcfFile = new File(Global.HBaseStorage.get("gene").toString());
+		String filePath = new String("/Users/bso/output2.txt");
+		
+		//Initialize query stores to dump queries from input JSON
 		HashMap<String, String> fmapQuery = new HashMap<String,String>();
 		HashMap<String, String> fsmapQuery = new HashMap<String,String>();
 		HashMap<String, String> regmapQuery = new HashMap<String,String>();
-
 
 		/**	"OutKey":
 		{
 			"InKey": "jsonObInner.get(InKey)"
 		}*/
+		
 		while (OutterKeys.hasNext()){
 			String OutKey = OutterKeys.next();
+			
 			if (jsonObOuter.get(OutKey) instanceof JSONObject){
 				JSONObject jsonObInner = jsonObOuter.getJSONObject(OutKey);
 				Iterator<String> InnerKeys = jsonObInner.keys();
+				
 				while (InnerKeys.hasNext()){
 					String InKey = InnerKeys.next();
-					//Save values of 
+					
 					if (OutKey.equals("feature_sets")){
-						fsmapQuery.put(InKey.toString(), jsonObInner.getString(InKey));
+						fsmapQuery.put(InKey.toString(), 
+										jsonObInner.getString(InKey));
 					}
+					
 					if (OutKey.equals("features")){
-						fmapQuery.put(InKey.toString(), jsonObInner.getString(InKey));
+						fmapQuery.put(InKey.toString(), 
+										jsonObInner.getString(InKey));
 					}
 				}
 			} else if (jsonObOuter.get(OutKey) instanceof JSONArray){
 				JSONArray jsonArInner = jsonObOuter.getJSONArray(OutKey);
 					if(OutKey.equals("regions")){
 						regionArray = jsonObOuter.getJSONArray(OutKey);
+						
 						for (int i=0; i< regionArray.length(); i++){
-							String region = regionArray.get(i).toString();
+							String region = regionArray
+											.get(i)
+											.toString();
+							
 							if (region.contains(":") == false){
-								String chromosomeID = region.substring(region.indexOf("r")+1, region.length());//selects "22" from "chr22"
-								regmapQuery.put(chromosomeID.toString(), ".");
+								
+								//i.e. selects "22" from "chr22"
+								String chromosomeID = region.substring(
+										region.indexOf("r")+1,
+										region.length());
+								
+								regmapQuery.put(chromosomeID.toString(), 
+												".");
+								
 							} else if (region.contains(":") == true){
-								String chromosomeID = region.substring(region.indexOf("r")+1 , region.indexOf(":")); //selects "22" from "chr22:1-99999"
-								String range = region.substring(region.indexOf(":")+1, region.length());
-								regmapQuery.put(chromosomeID.toString(), range.toString());
+								
+								//i.e. selects "22" from "chr22:1-99999"
+								String chromosomeID = region.substring(
+										region.indexOf("r")+1,
+										region.indexOf(":"));
+								
+								String range = region.substring(
+										region.indexOf(":")+1,
+										region.length());
+								
+								regmapQuery.put(chromosomeID.toString(), 
+												range.toString());
 							}
 						}
 					}
 			}
 		}
 		
-		File sortedVcfFile = new File(Global.HBaseStorage.get("gene").toString());
-		String filePath = new String("/Users/bso/output2.txt");
-		
-		//Initialize reader
+		//Initialize comparison process
 		Iterator<VariantContext> vcfIterator;
 		{
+			//Initialize reader and writer
+		    PrintWriter writer = new PrintWriter(filePath, "UTF-8");
 		    final VCFCodec vcfCodec = new VCFCodec(); //declare codec
 		    final boolean requireIndex = false; //index not required
-		    
 		    BufferedReader vcfReader = new BufferedReader(new FileReader(sortedVcfFile));
 		    FeatureReader<VariantContext> reader = getFeatureReader(sortedVcfFile, vcfCodec, requireIndex);
 		    vcfIterator = reader.iterator();
 		    VCFHeader header = (VCFHeader) reader.getHeader();
-		    
-		    PrintWriter writer = new PrintWriter(filePath, "UTF-8");
-		    
-		    String noMatch = new String();
-		    String temp= new String();
-		    String chrPairsTempasString = new String();
-		    String chrID = new String();
-		    List<Map> sorted = new ArrayList<Map>();
-		    
+
+		    //Initialize variables for query checking
 		    int Querysize; //Points needed for a variant Attribute (totaled through all features, and one from region) to be VALID
 		    int fieldCounter;
 		    int upperBound; //Region query
 		    int lowerBound;
-		    int variantChr; 
-		    //check attributes for hits
+		    int variantChr;
+		    List<Map> sorted = new ArrayList<Map>();
+		    String chrPairsTempasString = new String();
+		    String chrID = new String();
+		    String noMatch = new String();
+		    String temp= new String();
 
 		    //Determine if user has input a chromosome query
 		    if (regmapQuery.size() >= 1){
@@ -200,30 +227,49 @@ public class TestBackends implements BackendTestInterface
 		    /**BEGIN LOOPING OF EVERY VARIANT LINE TO MATCH FOR QUERY RESULTS
 		     * 
 		     */
-			while (vcfIterator.hasNext()){ //loop through each variant
-			    Iterator regmapIterator = regmapQuery.entrySet().iterator();
-				Iterator fmapIterator = fmapQuery.entrySet().iterator(); //iterate over fmapQuery
-				VariantContext variantContext = vcfIterator.next();
+			while (vcfIterator.hasNext()){ //loop through each VARIANT
+			    Iterator regmapIterator = regmapQuery
+		    							.entrySet()
+		    							.iterator();
+			    
+				Iterator fmapIterator = fmapQuery
+										.entrySet()
+										.iterator(); //iterate over fmapQuery
 				
+				VariantContext variantContext = vcfIterator.next();
 				fieldCounter = 0; //Reset the field counter
+				
 				while(regmapIterator.hasNext()){//loop through each query in chromosomes
 					Map.Entry chrPairs = (Map.Entry)regmapIterator.next();
 					
 					//GATHER FIRST POINT FROM MATCHING CHROMOSOME NUMBER AND RANGE IN QUERY
 					if (chrPairs.getKey().equals(variantContext.getChr())){ //check if query in chromosomes matches current chrID in variant
-						chrPairsTempasString = chrPairs.getValue().toString();
+						chrPairsTempasString = chrPairs
+								.getValue()
+								.toString();
+						
 						if (chrPairsTempasString.equals(".") == true){ //Checks if the current variant contains ALL POS
 							fieldCounter++;
-							chrID = chrPairs.getKey().toString();
+							chrID = chrPairs
+									.getKey()
+									.toString();
+							
 						} else if (chrPairsTempasString.equals(".") == false) {
 								
-								lowerBound = Integer.parseInt(chrPairsTempasString.substring(0,chrPairsTempasString.indexOf("-")));
-								upperBound = Integer.parseInt(chrPairsTempasString.substring(chrPairsTempasString.indexOf("-")+1, chrPairsTempasString.length()));
+								lowerBound = Integer.parseInt(chrPairsTempasString.substring(
+										0,chrPairsTempasString.indexOf("-")));
+								
+								upperBound = Integer.parseInt(chrPairsTempasString.substring(
+										chrPairsTempasString.indexOf("-")+1, 
+										chrPairsTempasString.length()));
+								
 								variantChr = variantContext.getStart();
 
 								if (variantChr >= lowerBound && variantChr <= upperBound){ //checks if current variant POS is within specified range
 									fieldCounter++;
-									chrID = chrPairs.getKey().toString();
+									chrID = chrPairs
+											.getKey()
+											.toString();
 								}
 						}
 					}
@@ -231,8 +277,16 @@ public class TestBackends implements BackendTestInterface
 					//GATHER THE REST OF THE POINTS FROM MATCHING ALL THE FEATURES IN QUERY
 				    while (fmapIterator.hasNext()) { //loop through each query in features
 				        Map.Entry pairs = (Map.Entry)fmapIterator.next();
-				        String variantAttribute = variantContext.getAttributeAsString(pairs.getKey().toString(), noMatch);
-				        String queryAttribute = pairs.getValue().toString();
+				        
+				        String variantAttribute = variantContext
+				        		.getAttributeAsString(
+				        				pairs.getKey().toString(), 
+				        				noMatch);
+				        
+				        String queryAttribute = pairs
+				        		.getValue()
+				        		.toString();
+				        
 				        if (variantAttribute.equals(pairs.getValue().toString())){				        	
 				        	fieldCounter++; //Accumulate points
 				        }
@@ -243,16 +297,24 @@ public class TestBackends implements BackendTestInterface
 				    **/
 				    
 				    if (fieldCounter == Querysize){
-				    	Iterator attributeIter = variantContext.getAttributes().entrySet().iterator();
+				    	Iterator attributeIter = variantContext
+				    							.getAttributes()
+				    							.entrySet()
+				    							.iterator();
+				    	
 				    	String attributeSorted = new String();
 				    	String attributeSortedHolder = new String();
 				    	
-				    	while(attributeIter.hasNext()){ //Resort the info field from a map to match VCF format
+				    	//Resort the info field from a map format to match VCF format
+				    	while(attributeIter.hasNext()){ 
 				    		Map.Entry pair = (Map.Entry)attributeIter.next();
-				    		attributeSortedHolder = pair.getKey().toString() + "=" + pair.getValue().toString() + ";";
+				    		
+				    		attributeSortedHolder = pair.getKey().toString() + "=" + 
+				    								pair.getValue().toString() + ";";
+				    		
 				    		attributeSorted = attributeSorted + attributeSortedHolder;
 				    	}
-				    					    	
+				    	
 				    	//Write variant to file
 				    	writer.println("chr" + chrID + "\t" +
 				    					variantContext.getEnd() + "\t" +
@@ -268,7 +330,9 @@ public class TestBackends implements BackendTestInterface
 		return finished;
 	}
 	
-	private FeatureReader<VariantContext> getFeatureReader(File sVcfFile, final VCFCodec vcfCodec, final boolean requireIndex) {
+	private FeatureReader<VariantContext> getFeatureReader(File sVcfFile, 
+															final VCFCodec vcfCodec, 
+															final boolean requireIndex) {
 		return AbstractFeatureReader.getFeatureReader(sVcfFile.getAbsolutePath(), vcfCodec, requireIndex);
 	}
 
