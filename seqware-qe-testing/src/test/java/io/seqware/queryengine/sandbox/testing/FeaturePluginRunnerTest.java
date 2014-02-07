5676570a3f4f0b14cd1f21ca754e4f8c2d128603
bcso <bcso@uwaterloo.ca>
2014-02-07 16:33:11 -0500

    Added more notes

--- seqware-qe-testing/src/test/java/io/seqware/queryengine/sandbox/testing/FeaturePluginRunnerTest.java
+++ seqware-qe-testing/src/test/java/io/seqware/queryengine/sandbox/testing/FeaturePluginRunnerTest.java
@@ -13,15 +13,22 @@ import io.seqware.queryengine.sandbox.testing.plugins.*;
 
 public class FeaturePluginRunnerTest {
 	@Test
+	//This is basically and external implementation of the runPlugin function in BackendTestInterface, for ease of testing.
+	//Will be added into GATK_Picard_Backend test.
 	public void runPlugin(/**There will be a json input here + the plugin authors plugin class**/) throws IOException, JSONException{
 		FeaturePluginRunner runMe = new FeaturePluginRunner();
+		
+		//Move these to some external global variable later, will not be called in the runPlugin params
+		String outputDir = System.getProperty("user.home") + "/Filtered";
+		String inputDir = "src/main/resources/PluginData/";
 		
 		File jsonQuery = new File("src/main/resources/testdata/query.json");
 		InputStream is = new FileInputStream(jsonQuery);
 		String jsonTxt = IOUtils.toString(is);
 		
+		//Filtering commences
+		runMe.getFilteredFiles(inputDir, jsonTxt, outputDir);
 		
-		runMe.getFilteredFiles("src/main/resources/PluginData/", jsonTxt, System.getProperty("user.home") + "/Filtered");
 		//TODO: run map for every genome position
 	}
 	
