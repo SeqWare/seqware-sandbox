6915b54cebf5827c0fda9101965d6f1da78c9ff8
Adam Wright <adam.wright@oicr.on.ca>
2014-10-22 19:06:12 +0000

    Add support to automatically run the generator to generate a JSON file to be used as input by the ansible inventory file script.
    Typical usage:
    
    python host_inventory_form_json.py --run_generator --generator_path /home/vagrant/git/youxia/youxia-generator/target/youxia-generator-*-jar-with-dependencies.jar test.ini

--- ansible_pancancer/host_inventory_form_json.py
+++ ansible_pancancer/host_inventory_form_json.py
@@ -3,19 +3,33 @@ from os import path as path
 import re
 from subprocess import check_output, Popen, PIPE
 import json
+from os import remove
 
 parser = argparse.ArgumentParser(description="Convert a Seqware decider JSON file for BWA to an ansible host inventory file")
-parser.add_argument("input_file_path", help="Full path to the input JSON file")
+parser.add_argument("--run_generator", help="Enabling this option automatically runs the generator and passes the standard out of the generator to the program as input. If you enable this option you should indicate the path to the generator JAR via the --generator_path argument.", action="store_true")
+parser.add_argument("--generator_path", help="Path to the generator JAR") 
+parser.add_argument("--input_file_path", help="Full path to the input JSON file")
 parser.add_argument("output_file_path", help="Full path to the inventory host file to be generated")
 args = parser.parse_args()
 arg_dict = vars(args)
 
+run_generator = arg_dict["run_generator"]
+generator_path = arg_dict["generator_path"]
 input_file_path = arg_dict["input_file_path"]
 output_file_path = arg_dict["output_file_path"]
+
+tmp_json_filename = "generator_tmp.json"
+
+if run_generator:
+	(prog_out, prog_err) = Popen(["java", "-jar", generator_path, "--aws", "--output", tmp_json_filename],stdin=PIPE, stdout=PIPE, stderr=PIPE).communicate()
+	input_file_path = tmp_json_filename
 
 infile = open(input_file_path, "r")
 json_text = json.load(infile)
 infile.close()
+
+if run_generator:
+	remove(tmp_json_filename)
 
 outfile = open(output_file_path, "w")
 outfile.write("[all-masters]\n")
