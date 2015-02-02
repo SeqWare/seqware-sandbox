d9feef2a8c9f1d3bd484359c4f564087668d44c2
Denis Yuen <dyuen@oicr.on.ca>
2015-02-02 13:56:55 -0500

    Postgres not starting

--- full_seqware_inside_docker/launcher/scripts/start.sh
+++ full_seqware_inside_docker/launcher/scripts/start.sh
@@ -2,17 +2,7 @@
 
 # kick off all services
 
-sudo service tomcat7 start
-sudo service postgresql start
-sudo service hadoop-hdfs-datanode start
-sudo service hadoop-hdfs-namenode start
-sudo service hadoop-0.20-mapreduce-jobtracker start
-sudo service hadoop-0.20-mapreduce-tasktracker start
-sudo service oozie start 
-sudo service gridengine-master start 
-sudo service gridengine-exec start 
+ansible-playbook /mnt/home/seqware/seqware-bag/docker-start.yml -c local --extra-vars "single_node=True"
 sudo nohup cron -f &
 cd ~seqware
 /bin/bash
-
-
